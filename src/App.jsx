6b5c64053e308d04c6c9a56d065632fdc38629b3
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-12-16 17:03:26 -0600

    still working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Nav from "./Nav";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
   return (
     <>
-      <div></div>
+      <div className="App">
+        <RouterProvider router={router} />
+        <Nav />
+        <Header />
+        <Footer />
+      </div>
     </>
   );
 }
